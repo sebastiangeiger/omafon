20f9863d4c8b2737386fff3a7ee2554312575675
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-06-18 10:11:26 -0700

    Extending infrastructure test to WebSockets

--- backend/spec/acceptance/sign_in_and_see_contacts_spec.rb
+++ backend/spec/acceptance/sign_in_and_see_contacts_spec.rb
@@ -1,14 +1,21 @@
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 require_relative '../../../frontend/file_server'
+require_relative '../../app/server'
 
 Capybara.app = Sinatra::Application
 Capybara.javascript_driver = :poltergeist
 
 describe "Sign in and see online contacts", type: :feature, js: true do
+  let(:users) { UserCollection.new }
+  let(:domain_model) { DomainModel.new(users: users) }
+  let(:server) { Server.new }
+  after(:each) { server.kill }
   it 'does something' do
+    server.start(domain_model)
     visit '/'
     expect(page).to have_content 'Hello from HTML'
     expect(page).to have_content 'Hello from JS'
+    expect(page).to have_content 'Hello from WebSockets'
   end
 end
--- frontend/application.js
+++ frontend/application.js
@@ -1,3 +1,7 @@
 window.onload = function() {
   document.body.innerHTML += '<div>Hello from JS</div>'
+  var socket = new WebSocket("ws://localhost:8080")
+  socket.onopen = function (event) {
+    document.body.innerHTML += '<div>Hello from WebSockets</div>'
+  };
 };
