59bfd4c7f4cd266f99ee65f85a5eec234f958e38
Sebastian Geiger <sebastian.geiger@gmail.com>
2014-06-17 20:52:47 -0700

    Renaming OutgoingMessageQueue to Postmaster

--- backend/app/models/domain_model.rb
+++ backend/app/models/domain_model.rb
@@ -3,7 +3,7 @@ require_relative 'session'
 require_relative 'connection'
 require_relative '../monkeypatches'
 require_relative '../message_handlers/message_handler'
-require_relative 'outgoing_message_queue'
+require_relative 'postmaster'
 require_relative '../message_handlers/message_handler_executor'
 require_relative '../my_logger'
 
@@ -14,14 +14,11 @@ class DomainModel
     @sessions = SessionCollection.new
     @connections = ConnectionCollection.new
     @authenticator = Authenticator.new(@sessions)
-    @outgoing_messages = OutgoingMessageQueue.new(@sessions,@connections)
+    @postmaster = Postmaster.new(@sessions,@connections)
     @log = MyLogger.new
   end
   def incoming_message(message,connection)
     process_incoming_message(message.symbolize_keys,connection)
-  end
-  def empty_messages
-    @outgoing_messages.clear
   end
   def create_connection
     @connections.create_connection(self)
@@ -32,8 +29,8 @@ class DomainModel
     handler = MessageHandler.get_handler(message)
     executor = MessageHandlerExecutor.new(handler,message,connection,self)
     executor.execute!
-    @outgoing_messages.add(executor.outgoing_messages)
-    @outgoing_messages.deliver_messages!
+    @postmaster.add_messages(executor.outgoing_messages)
+    @postmaster.deliver_messages!
   end
 
 end
--- backend/app/models/postmaster.rb
+++ backend/app/models/postmaster.rb
@@ -1,26 +1,21 @@
 require_relative '../my_logger'
 
-class OutgoingMessageQueue
+class Postmaster
   def initialize(sessions,connections)
     @messages = []
     @log = MyLogger.new
     @sessions = sessions
     @connections = connections
   end
-  def to_a
-    @messages
-  end
-  def add(new_messages)
+
+  def add_messages(new_messages)
     new_messages.map! do |message|
       correct_recipients(message.to_hash)
     end
     @log.debug "Added messages: #{new_messages}"
     @messages += new_messages
   end
-  def clear
-    @log.debug "Cleared the OutgoingMessageQueue" unless @messages.empty?
-    @messages.clear
-  end
+
   def correct_recipients(message)
     if recipient = message.delete(:recipient)
       message[:recipients] = [recipient]
@@ -30,9 +25,11 @@ class OutgoingMessageQueue
     end
     message
   end
+
   def deliver_messages!
     @messages.each {|msg| deliver_message(msg)}
   end
+
   def deliver_message(message)
     recipients = message.delete(:recipients) || []
     recipients.each do |recipient|
